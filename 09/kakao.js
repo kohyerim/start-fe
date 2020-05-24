cc01c19efcd519b9cf9254b35172643114f26b7b
kohyerim <kohyerim78@gmail.com>
2020-05-24 10:28:45 +0900

    09 과제

--- 09/kakao.js
+++ 09/kakao.js
@@ -1,18 +1,34 @@
+/* eslint-disable no-alert */
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
 const $result = document.querySelector('#result');
+const $more = document.querySelector('#more');
 const url = 'https://dapi.kakao.com/v2/search/web';
 const headers = { Authorization: 'KakaoAK 2cc871a74c9e1ff6e83791a3703b9aaf' };
+let pageNum = 1;
 
 function success(data) {
   const { documents } = data;
   const li = documents.map((doc) => {
-    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
+    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.title}</a></li>`;
   });
 
   $result.innerHTML = `<ul class="list-group list-group-flush">${li.join(
     '',
   )}</ul>`;
+  $more.disabled = false;
+}
+
+function moreInfo(data) {
+  const { documents } = data;
+  const li = documents.map((doc) => {
+    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.title}</a></li>`;
+  });
+
+  $result.innerHTML += `<ul class="list-group list-group-flush">${li.join(
+    '',
+  )}</ul>`;
+  $more.disabled = false;
 }
 
 function error() {
@@ -22,7 +38,6 @@ function error() {
 function search() {
   const { value } = $text;
   if ($text.value === '') {
-    // eslint-disable-next-line no-alert
     alert('검색어를 입력하세요');
     return false;
   }
@@ -33,4 +48,15 @@ function search() {
   return true;
 }
 
+function more() {
+  pageNum += 1;
+  const { value } = $text;
+  fetch(`${url}?query=${value}&page=${pageNum}`, { headers })
+    .then((res) => res.json())
+    .then(moreInfo)
+    .catch(error);
+  return true;
+}
+
 $btn.addEventListener('click', search);
+$more.addEventListener('click', more);
