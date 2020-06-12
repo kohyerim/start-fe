d33545ced22e0d54759d93f23bc1e898230e8886
kohyerim <kohyerim78@gmail.com>
2020-06-12 20:21:59 +0900

    add docs

---
+++ 11/src/data-manager.js
@@ -0,0 +1,17 @@
+function saveData(data) {
+  console.log('todos : ', data);
+
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+function loadData(callback) {
+  // const dataString = localStorage.getItem('data') || '[]';
+  // return JSON.parse(dataString);
+  fetch('http://localhost:3000/todos').then((response) => {
+    response.json().then((data) => {
+      callback(data);
+    });
+  });
+}
+
+export { saveData, loadData };
--- 11/src/index.js
+++ 11/src/index.js
@@ -1,23 +1,25 @@
 import { render } from './html-render';
 import formInput from './form-input';
+import { loadData } from './data-manager';
 
 import './todos.css';
 
 const $result = document.querySelector('#result');
 
-const todos = [];
+loadData((todos) => {
+  $result.addEventListener('click', (event) => {
+    const { className } = event.target;
+    if (className === 'delete') {
+      const { index } = event.target.parentElement.dataset;
+      todos.splice(index, 1);
+      render(todos);
+    } else if (className === 'toggle-checked') {
+      // const { index } = event.target.parentElement.dataset;
+      // todos[index].isDone = !todos[index].isDone;
+      // render(todos);
+    }
+  });
 
-$result.addEventListener('click', (event) => {
-  const { className } = event.target;
-  if (className === 'delete') {
-    const { index } = event.target.parentElement.dataset;
-    todos.splice(index, 1);
-    render(todos);
-  } else if (className === 'toggle-checked') {
-    const { index } = event.target.parentElement.dataset;
-    todos[index].isDone = !todos[index].isDone;
-    render(todos);
-  }
+  formInput.init(todos);
+  render(todos);
 });
-formInput.init(todos);
-render(todos);
